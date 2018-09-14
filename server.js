700eaf58f9c6a950d3f0c41ad41539e4b862b991
Benazir Fateh <fatehbenazir@gmail.com>
2018-09-14 06:29:56 +0530

    change project name in server.js to make it work on heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/GeoLocation'));
+app.use(express.static('./dist/Geolocation'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/GeoLocation/index.html'));
+res.sendFile(path.join(__dirname+'/dist/Geolocation/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
